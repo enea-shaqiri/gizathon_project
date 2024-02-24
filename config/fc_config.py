db7b54a1e99130bd13fb5018329eacea40bb65b3
enea <enea105@hotmail.it>
2024-02-24 14:17:05 +0100

    Added fully connected network configs

---
+++ config/fc_config.py
@@ -0,0 +1,8 @@
+configs = {"lookback_window": 12,
+           "hidden_size_1": 128,
+           "hidden_size_2": 64,
+           "hidden_size_3": 32,
+           "n_epochs": 100,
+           "lr": 0.001,
+           "batch_size": 16,
+           "filename": "fc_model.onnx"}
