3dc03c89aa4f438394be59df616bd4b448c6a826
enea <enea105@hotmail.it>
2024-02-24 16:15:15 +0100

    Training for longer

--- config/fc_config.py
+++ config/fc_config.py
@@ -2,7 +2,7 @@ configs = {"lookback_window": 12,
            "hidden_size_1": 128,
            "hidden_size_2": 64,
            "hidden_size_3": 32,
-           "n_epochs": 100,
+           "n_epochs": 400,
            "lr": 0.001,
            "batch_size": 16,
            "filename": "fc_model.onnx"}
